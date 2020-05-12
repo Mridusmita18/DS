2bdcc75b05306f57c97aacc12f094e85fafc84a5
Mridusmita Deka <mridusmita.050@gmail.com>
2020-05-12 16:48:50 +0530

    Commit all

---
+++ Learn_DS/IEnumerator_IEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn_DS
+{
+    static class IEnumerator_IEnumerable
+    {
+
+        public static IEnumerable<int> GetItems()
+        {
+            List<int> list = new List<int>()
+        {
+            10,20,60,90,100
+        };
+
+            IEnumerable<int> ienumerablelist = (IEnumerable<int>)list;
+            foreach (int i in ienumerablelist)
+                Console.WriteLine(i);
+            IEnumerator<int> ienumeratorlist = list.GetEnumerator();
+            while (ienumeratorlist.MoveNext())
+                Console.WriteLine(ienumeratorlist.Current);
+
+            foreach (int i in list)
+            {
+                if (i > 20)
+                    yield return i;
+            }
+        }
+    }
+}
--- Learn_DS/Program.cs
+++ Learn_DS/Program.cs
@@ -4,20 +4,27 @@ namespace Learn_DS
 {
     class Program
     {
-        static string Main(string[] args)
+        static void Main(string[] args)
         {
             Node first = new Node{ Value = 10 };
             Node second = new Node { Value = 50 };
+            Node third = new Node { Value = 100 };
             first.Next = second;
+            second.Next = third;
 
-           
+            //IEnumerator_IEnumerable.GetItems();
 
-            Console.WriteLine("Hello World!");
-            return "Hello";
+            //Print(first);
+
         }
 
         public static void Print(Node node)
         {
+            while(node!=null)
+            {
+                Console.WriteLine($"Value is : {node.Value}");
+                node = node.Next;
+            }
 
         }
     }
